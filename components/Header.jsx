78bc23e404a812a7146ec69d874dded447669b4e
Brandon <bmushori@gmail.com>
2022-04-27 07:41:47 +0200

    Update

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,15 +10,15 @@ import {
 } from "@heroicons/react/solid"
 
 const style = {
-  header: `sticky top-0 z-50 grid grid-cols-3 bg-white p-5 py-5 px-5 shadow-md md:px-10`,
+  header: `sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10`,
   logoContainer: `relative my-auto flex h-10 cursor-pointer items-center`,
-  searchWrapper: `flex items-center rounded-full py-2 md:border-2`,
+  searchWrapper: `flex items-center rounded-full py-2 md:border-2 md:shadow-lg`,
   searchIcon: `hidden h-8 cursor-pointer rounded-full bg-blue-300 p-2 text-white md:mx-2 md:inline-flex`,
   searchInput: `flex-grow bg-transparent pl-5 placeholder-gray-400 outline-none`,
   menuWrapper: `flex items-center justify-end space-x-4 text-gray-500`,
   paragraph: `hidden cursor-pointer md:inline`,
   menuItems: `flex items-center space-x-2 rounded-full border-2 p-2`,
-  globeAltIcon: `h-6 cursor-pointer hover:animate-spin`,
+  globeAltIcon: `h-6 cursor-pointer hover:animate-spin sm-hidden`,
 }
 
 function Header() {
